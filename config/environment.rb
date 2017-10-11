8b32b301859a7817f66549aef8efb6e18801ff14
Uliana25 <ulianazhernokleva@gmail.com>
2017-10-11 20:31:57 +0300

    mailer send

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,4 @@
-# Initialize the Rails application.
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['Uliana25'],
-  :password => ENV['uliana25'],
-  :domain => 'https://mighty-savannah-53637.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# Load the Rails application.
+ require_relative 'application'
+ # Initialize the Rails application.
+ Rails.application.initialize!
